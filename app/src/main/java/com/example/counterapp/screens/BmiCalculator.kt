d3283a959d39901312ee805bfefad9ccfaacb8a8
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-01-09 16:18:53 +0100

    bmi calculator done

--- app/src/main/java/com/example/counterapp/screens/BmiCalculator.kt
+++ app/src/main/java/com/example/counterapp/screens/BmiCalculator.kt
@@ -1,6 +1,8 @@
 package com.example.counterapp.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -8,6 +10,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -17,8 +20,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,6 +45,7 @@ fun BmiCalculator(modifier: Modifier = Modifier) {
         var weight by remember { mutableStateOf("") }
         var height by remember { mutableStateOf("") }
         var bmi by remember { mutableStateOf("") }
+        var status by remember { mutableStateOf("") }
 
         Column(
             modifier = modifier.padding(it)
@@ -69,10 +76,15 @@ fun BmiCalculator(modifier: Modifier = Modifier) {
                     weight = weight.toDoubleOrNull() ?: 0.0,
                     height = height.toDoubleOrNull() ?: 0.0
                 )
+                status = getBMIStatus(bmi.toDouble())
             }) {
                 Text(text = "Calculate BMI")
             }
-            Text(text = "BMI: $bmi")
+            BmiResult(
+                bmi = bmi,
+                status = status,
+                modifier = modifier
+            )
         }
     }
 }
@@ -80,6 +92,19 @@ fun BmiCalculator(modifier: Modifier = Modifier) {
 fun calculateBmi(weight: Double, height: Double): String {
     val bmi = weight / ((height / 100) * (height / 100))
     return String.format("%.1f", bmi)
+}
+
+fun getBMIStatus(bmi: Double): String {
+    return when (bmi) {
+        in 0.0..16.0 -> BMIConstants.SEVERELY_UNDERWEIGHT
+        in 16.0..16.9 -> BMIConstants.UNDERWEIGHT_MODERATE
+        in 17.0..18.4 -> BMIConstants.UNDERWEIGHT
+        in 18.5..24.9 -> BMIConstants.NORMAL_WEIGHT
+        in 25.0..29.9 -> BMIConstants.OVERWEIGHT
+        in 30.0..34.9 -> BMIConstants.OBESE_CLASS_I
+        in 35.0..39.9 -> BMIConstants.OBESE_CLASS_II
+        else -> BMIConstants.OBESE_CLASS_III
+    }
 }
 
 @Composable
@@ -100,8 +125,68 @@ fun EditNumberField(
     )
 }
 
+@Composable
+fun BmiResult(
+    bmi: String,
+    status: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "BMI: $bmi",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        if (status.isNotBlank()) {
+            for (key in statusMap.keys) {
+                val color = if (status == key) Color.DarkGray else Color.Transparent
+                val fontWeight = if (status == key) FontWeight.Bold else FontWeight.Normal
+                Row(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                        .fillMaxWidth()
+                        .background(color = color)
+                ) {
+                    Text(
+                        text = key,
+                        fontWeight = fontWeight,
+                        modifier = Modifier.weight(1f)
+                    )
+                    Text(text = statusMap[key] ?: "", fontWeight = fontWeight)
+                }
+            }
+        }
+    }
+}
+
 @Preview
 @Composable
 private fun BmiCalculatorPreview() {
     BmiCalculator()
 }
+
+object BMIConstants {
+    const val SEVERELY_UNDERWEIGHT = "Severely Underweight"
+    const val UNDERWEIGHT_MODERATE = "Moderate Underweight"
+    const val UNDERWEIGHT = "Underweight"
+    const val NORMAL_WEIGHT = "Normal weight"
+    const val OVERWEIGHT = "Overweight"
+    const val OBESE_CLASS_I = "Obesity Class I"
+    const val OBESE_CLASS_II = "Obesity Class II"
+    const val OBESE_CLASS_III = "Obesity Class III"
+}
+
+val statusMap = mapOf(
+    BMIConstants.SEVERELY_UNDERWEIGHT to "Less than 16.0",
+    BMIConstants.UNDERWEIGHT_MODERATE to "16.0 - 16.9",
+    BMIConstants.UNDERWEIGHT to "17.0 - 18.4",
+    BMIConstants.NORMAL_WEIGHT to "18.5 - 24.9",
+    BMIConstants.OVERWEIGHT to "25.0 -29.9",
+    BMIConstants.OBESE_CLASS_I to "30.0 - 34.9",
+    BMIConstants.OBESE_CLASS_II to "35.0 - 39.9",
+    BMIConstants.OBESE_CLASS_III to "40 and above"
+)
