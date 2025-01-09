ef5d8429e4155c46561c445566678cba5b38b0a1
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-01-09 20:40:28 +0100

    add viewmodel dependency

--- app/src/main/java/com/example/counterapp/viewmodels/CounterViewModel.kt
+++ app/src/main/java/com/example/counterapp/viewmodels/CounterViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.counterapp.viewmodels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class CounterViewModel: ViewModel() {
+    var counter by mutableStateOf(0)
+
+    fun incrementCounter() {
+        counter++
+    }
 }
